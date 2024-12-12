2b8528a4da43c5bc2de6706c670769c7cf239769
luohang09 <luohang09@meituan.com>
2024-12-12 14:26:26 +0800

    middleware

--- midleware-test/src/app.controller.ts
+++ midleware-test/src/app.controller.ts
@@ -29,19 +29,23 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  // 使用 @Next 装饰器注入 next 函数
+  // 使用 @Response 装饰器注入 response 对象,并设置 passthrough 为 true
   @Get('aaa')
-  a1(@Next() next, @Response({passthrough: true}) response) {
-    return 'hello'
+  a1(@Next() next, @Response({ passthrough: true }) response) {
+    return 'hello';
   }
 
+  // 使用 @Next 装饰器注入 next 函数
+  // 调用 next() 后继续执行并返回 hello1
   @Get('bbb')
   b1(@Next() next) {
     next();
-    return 'hello1'
+    return 'hello1';
   }
 
   @Get('bbb')
   b2() {
-    return 'hello2'
+    return 'hello2';
   }
 }
