58c13357233498f2f2b12b26dcf1a3b67592ce7d
luohang09 <luohang09@meituan.com>
2024-12-12 14:43:36 +0800

    切换 express 到 fastify

--- fastify-test/src/app.controller.ts
+++ fastify-test/src/app.controller.ts
@@ -3,17 +3,21 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 @Controller()
 export class AppController {
-  constructor() {}
-
   @Get()
-  getHello(@Request() request: FastifyRequest, @Response({passthrough: true}) reply: FastifyReply) {
-    reply.header('url', request.url)
+  getHello(
+    @Request() request: FastifyRequest,
+    @Response({ passthrough: true }) reply: FastifyReply,
+  ) {
+    reply.header('url', request.url);
     return 'hello';
   }
 
   @Get('aaa')
-  getHello2(@Request() request: FastifyRequest, @Response() reply: FastifyReply) {
-    reply.header('url', request.url)
-    reply.send('hello')
+  getHello2(
+    @Request() request: FastifyRequest,
+    @Response() reply: FastifyReply,
+  ) {
+    reply.header('url', request.url);
+    reply.send('hello');
   }
 }
